57366ff1d17ae899e11347c7a32b5e5520a7bdab
Jacob Schaupp <jacob.schaupp@rockets.utoledo.edu>
2018-11-20 00:26:01 -0500

    update

--- AVL_BTrees/AVL.cpp
+++ AVL_BTrees/AVL.cpp
@@ -11,41 +11,87 @@ AVL::AVL()
 
 void AVL::Insert(char in_key[])
 {
-	AVL_Node *leaderNode, *laggerNode, *lastOutOfSpec; 
+	AVL_Node *leaderNode, *laggerNode, *lastOutOfSpec;
+	int displacement; 
 
-	lastOutOfSpec = nullptr; 
+	lastOutOfSpec = nullptr;
 	leaderNode = rootNode;
-	laggerNode = nullptr; 
+	laggerNode = nullptr;
 
-	while (leaderNode != nullptr)
+	//tree is empty 
+	if (rootNode == nullptr)
 	{
-		laggerNode = leaderNode;
-		if (leaderNode->BF != 0) lastOutOfSpec = leaderNode; //store last node whose BF != 0
-
-		if (strcmp(leaderNode->key, in_key) == 0)
-		{
-			leaderNode->counter++;
-			return;
-		}
-		else if (strcmp(leaderNode->key, in_key) > 0)
-		{
-			leaderNode = leaderNode->leftChild;
-		}
-		else leaderNode = leaderNode->rightChild;
+		AVL_Node* newNode = new AVL_Node();
+		strcpy(newNode->key, in_key);
+		newNode->leftChild = nullptr;
+		newNode->rightChild = nullptr; 
+		newNode->BF = 0;
+		newNode->counter = 1;
+		rootNode = newNode;
+		return;
 	}
-	//hit leaf position, lagger node is connection point
-	//create new node 
-
-	AVL_Node newNode;
-	newNode.counter = 1;
-	strcpy(newNode.key, in_key);
-
-	if (laggerNode == nullptr) rootNode = &newNode; 
-	else if (strcmp(laggerNode->key, in_key) > 0) laggerNode->leftChild = &newNode;  //new node is left child
-	else laggerNode->rightChild = &newNode; 
-
-	if (lastOutOfSpec != nullptr)
+	else
 	{
+		do
+		{
+			if (leaderNode != nullptr)
+			{
+				laggerNode = leaderNode;
+				if (leaderNode->BF != 0) lastOutOfSpec = leaderNode; //store last node whose BF != 0
 
+				if (strcmp(leaderNode->key, in_key) == 0)
+				{
+					leaderNode->counter++;
+					return;
+				}
+				else if (strcmp(leaderNode->key, in_key) > 0)
+				{
+					if (leaderNode->rightChild != nullptr) leaderNode = leaderNode->leftChild;
+					else leaderNode = nullptr; 
+				}
+				else if (leaderNode->rightChild != nullptr)leaderNode = leaderNode->rightChild;
+				else leaderNode = nullptr; 
+			}
+			else break; 
+		} while (leaderNode != nullptr);
+		//hit leaf position, lagger node is connection point
+		//create new node 
+
+		AVL_Node *newNode = new AVL_Node();
+		newNode->counter = 1;
+		strcpy(newNode->key, in_key);
+
+		if (laggerNode == nullptr) rootNode = newNode;
+		else if (strcmp(laggerNode->key, in_key) > 0) laggerNode->leftChild = newNode;  //new node is left child
+		else laggerNode->rightChild = newNode;
+
+		if (lastOutOfSpec != nullptr)
+		{
+			if (strcmp(in_key, lastOutOfSpec->key) > 0)
+			{
+				displacement = -1; 
+				lastOutOfSpec = lastOutOfSpec->rightChild; 
+			}
+			else
+			{
+				displacement = 1; 
+				lastOutOfSpec = lastOutOfSpec->leftChild; 
+			}
+
+			//adjust BFs
+			while (lastOutOfSpec != newNode)
+			{
+				if (strcmp(in_key, lastOutOfSpec->key) > 0)
+				{
+					lastOutOfSpec->BF = -1; 
+					lastOutOfSpec = lastOutOfSpec->rightChild; 
+				}
+				else
+				{
+					lastOutOfSpec->BF = 1; 
+					lastOutOfSpec = lastOutOfSpec->leftChild; 
+				}
+			}
+		}
 	}
 }
