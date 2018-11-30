0a28d17db926d4f1ef26fe8ed2194b09d211aa5e
Jacob Schaupp <jacob.schaupp@rockets.utoledo.edu>
2018-11-30 13:39:00 -0500

    wrote search method

--- AVL_BTrees/B_Tree.cpp
+++ AVL_BTrees/B_Tree.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "B_Tree.h"
+#include <string.h>
 
 
 B_Tree::B_Tree()
@@ -10,4 +11,28 @@ B_Tree::B_Tree()
 void B_Tree::Insert(char in_key[])
 {
 
+}
+
+//private
+
+void B_Tree::_traverse(char in_key[])
+{
+
+}
+
+bool B_Tree::_search(BNode *X, BNode *Y, char in_key[])
+{
+	bool found = false; 
+	int i = 1; 
+	while ((i < X->maxNumKey) && (strcmp(in_key, X->key[i]) > 0)) i++; 
+
+	//If found key is the same as input, return node
+	if (strcmp(in_key, X->key[i]) == 0) return true; 
+
+	if (X->leaf == true) return false; 
+	else
+	{
+		_readFile(X->child[i], X); 
+
+	}
 }
